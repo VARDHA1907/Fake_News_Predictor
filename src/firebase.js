5cc9d31e2561f8cdf6a00f9b9e51f02bf13ed131
SRIVARDHANI E <119473924+VARDHA1907@users.noreply.github.com>
2025-08-05 21:49:39 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDPFIhrL9iTi06wme8HUcm1B2Mc6IdoJnI",
+  apiKey: "API_KEY",
   authDomain: "fake-news-detection-app-b1ebe.firebaseapp.com",
   projectId: "fake-news-detection-app-b1ebe",
   storageBucket: "fake-news-detection-app-b1ebe.firebasestorage.app",
   messagingSenderId: "401082610166",
-  appId: "1:401082610166:web:3eaff7d806b781bf5257d7",
-  measurementId: "G-DMLJT0ZQ4W"
+  appId: "API_ID",
+  measurementId: "MEASUREMENT_ID"
 };
 
 // ✅ Initialize Firebase
